0ac12bd2d32fe6725e7a64c9d765eaba5f030161
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-02-07 14:21:03 +0200

    2021/01/26/07_41/22/backend/move-models-to-a-separate-module
    sub-steps:
    
    move the definition of token_serializer higher up in goal_tracker.py
    
    create a separate models.py module
    
    cleanup
    
    update launch.json and _temporarily_ break goal_tracker.py (for the sake of example)
    
    fix goal_tracker.py back

--- goal_tracker.py
+++ goal_tracker.py
@@ -4,7 +4,6 @@ from flask import Flask, request, jsonify, url_for
 from werkzeug.http import HTTP_STATUS_CODES
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from itsdangerous import (
     TimedJSONWebSignatureSerializer as Serializer,
@@ -13,6 +12,7 @@ from itsdangerous import (
 )
 
 from utils import format_time, parse_time
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_file = os.path.join(basedir, "goal_tracker.db")
@@ -29,52 +29,10 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
-
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-
-    goals = db.relationship("Goal", lazy="dynamic", backref="user")
-
-    def set_password_hash(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return f"<User {self.email}>"
-
-    def generate_token(self):
-        token = token_serializer.dumps({"user_id": self.id}).decode("utf-8")
-        return token
+token_serializer = Serializer(app.config["SECRET_KEY"], expires_in=3600)
 
 
-class Goal(db.Model):
-    __tablename__ = "goals"
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(256))
-
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-
-    intervals = db.relationship("Interval", lazy="dynamic", backref="goal")
-
-    def __repr__(self):
-        return f"<Goal '{self.description}'>"
-
-
-class Interval(db.Model):
-    __tablename__ = "intervals"
-    id = db.Column(db.Integer, primary_key=True)
-    start = db.Column(db.DateTime)  # TODO: consider adding `nullable=False`
-    final = db.Column(db.DateTime)
-
-    goal_id = db.Column(db.Integer, db.ForeignKey("goals.id"))
-
-    def __repr__(self):
-        return f"<Interval {self.id} (goal={self.goal})>"
+from models import User, Goal, Interval
 
 
 def error_response(status_code, message):
@@ -107,8 +65,6 @@ def basic_auth_error():
     r.headers["WWW-Authenticate"] = 'Bearer realm="Authentication Required"'
     return r
 
-
-token_serializer = Serializer(app.config["SECRET_KEY"], expires_in=3600)
 
 token_auth = HTTPTokenAuth(scheme="Bearer")
 
---
+++ models.py
@@ -0,0 +1,53 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+try:
+    from __main__ import db, token_serializer
+except ImportError:
+    from goal_tracker import db, token_serializer
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    goals = db.relationship("Goal", lazy="dynamic", backref="user")
+
+    def set_password_hash(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+    def generate_token(self):
+        token = token_serializer.dumps({"user_id": self.id}).decode("utf-8")
+        return token
+
+
+class Goal(db.Model):
+    __tablename__ = "goals"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(256))
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    intervals = db.relationship("Interval", lazy="dynamic", backref="goal")
+
+    def __repr__(self):
+        return f"<Goal '{self.description}'>"
+
+
+class Interval(db.Model):
+    __tablename__ = "intervals"
+    id = db.Column(db.Integer, primary_key=True)
+    start = db.Column(db.DateTime)  # TODO: consider adding `nullable=False`
+    final = db.Column(db.DateTime)
+
+    goal_id = db.Column(db.Integer, db.ForeignKey("goals.id"))
+
+    def __repr__(self):
+        return f"<Interval {self.id} (goal={self.goal})>"
